fa5ddde8e76ec4ce57043ce6f8f68183bc22bc23
Thiago Vazquez <thiagovazquez808@gmail.com>
2026-01-28 15:03:41 -0300

    Defini variables de intento, tambien la variable resultado dentro del while de intentos

--- wordle.py
+++ wordle.py
@@ -22,3 +22,23 @@ def verificador_palabra(palabra_ingresada, palabra_secreta):
         ## Y por ultimo un return que guarda los valores
     return letras_verificadas
 
+### Aca defini las variables de intentos y la de gano
+intentos = 0
+gano = False   
+
+## Imprimi la palabra wordle como decoracion
+
+print("WORDLE\n")
+
+# Aca defino el bucle while para los intentos
+
+while intentos < intentos_maximos:
+    print(f"Intento {intentos + 1} de {intentos_maximos}\n")
+    palabra_ingresada = input("Ingrese una palabra:\n")
+
+# Creo la variable de resultado que compara la palabra ingresada con la correcta
+
+    resultado = verificador_palabra(palabra_ingresada, palabra_secreta)
+    print("\nResultado:")
+    print(" ".join(resultado))
+    print("\n-----------------\n")
